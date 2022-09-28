99dbd7c110322298271cf57465fa8be0b23a56c8
Dylan Crowley <dcrowdev1025@gmail.com>
2022-09-28 18:25:02 -0400

    completely reworked functions

--- Assets/script.js
+++ Assets/script.js
@@ -29,60 +29,112 @@ var storedPassword = [];
 
 
 function criteriaSelect() {
-    let passwordLength = prompt("How long of a password do you want?");
-          if (passwordLength < 8 || passwordLength > 128) {
-              alert("Sorry, your password may only be between 8 and 128 characters.")
-            } else {
-              alert("You have decide to use a password length of " + passwordLength + ".");
-                specChars();
-                useUpperCase();
-                useLowerCase();
-                useNumbers();
+  // storedSpecChars.pop();
+  // storedLowerCaseLetters.pop();
+  // storedUpperCaseLetters.pop();
+  // storedNumbers.pop();
+     let passwordLength = prompt("How long of a password do you want?");
+        if (isNaN(passwordLength)) 
+          alert("Sorry, you may only input numbers!")
+        else if (passwordLength < 8 || passwordLength > 128) 
+           alert("Sorry, your password may only be between 8 and 128 characters.")
+        else {
+            alert("You have decide to use a password length of " + passwordLength + ".") 
+            specChars();
+        }
+
+      
+    
 
 
 
-                function specChars() {
-                  let storedSpecChars = prompt("Would you like to use Special Characters? Please enter 'Yes' or 'No'");
-                    if (storedSpecChars == "Yes") {
-                      storedPassword.push(specialCharacters[Math.floor(Math.random() * specialCharacters.length)]);
-                  }
-                }
 
-                
+function specChars() {
+  let storedSpecChars = prompt("Would you like to use Special Characters? Please enter 'Yes' or 'No'");
+  if (storedSpecChars == "Yes") {
+    storedPassword.push(specialCharacters[Math.floor(Math.random() * specialCharacters.length)]);
+    useUpperCase();
+  }
+  else if (storedSpecChars == "No") {
+    useUpperCase();
+  } else {
+    alert("Sorry, that input is invalid");
+  } return
 
 
-                function useUpperCase() {
-                  let storedUpperCaseLetters = prompt("Would you like to use Upper Case Letters? Please enter 'Yes' or 'No'");
-                    if (storedUpperCaseLetters == "Yes") {
-                      storedPassword.push(capitalLetters[Math.floor(Math.random() * capitalLetters.length)]);
-                  }
-                }
+
+
+
+  function useUpperCase() {
+    let storedUpperCaseLetters = prompt("Would you like to use Upper Case Letters? Please enter 'Yes' or 'No'");
+    if (storedUpperCaseLetters == "Yes") {
+      storedPassword.push(capitalLetters[Math.floor(Math.random() * capitalLetters.length)]);
+      useLowerCase();
+    } else if (storedUpperCaseLetters == "No") {
+      useLowerCase();
+    } else {
+        alert("Sorry, that input is invalid");
+    } return
+  }
+  
+
+
+  function useLowerCase() {
+    let storedLowerCaseLetters = prompt("Would you like to use Lower Case Letters? Please enter 'Yes' or 'No'");
+    if (storedLowerCaseLetters == "Yes") {
+      storedPassword.push(lowercaseLetters[Math.floor(Math.random() * lowercaseLetters.length)]);
+      useNumbers();
+    } else  if (storedLowerCaseLetters == "No") {
+      useNumbers();
+    } else {
+        alert("Sorry, that input is invalid");
+    }
+  }
+
+  function useNumbers() {
+    let storedNumbers = prompt("Would you like to use Numbers? Please enter 'Yes' or 'No'");
+    if (storedNumbers == "Yes") {
+      storedPassword.push(numbers[Math.floor(Math.random() * numbers.length)]);
+      generatePassword();
+    } else if (storedNumbers == "No") {
+      generatePassword();
+    } else {
+        alert("Sorry, that input is invalid");
+}
+  
+  
+
+function generatePassword() {
+    if (storedPassword.length !== passwordLength) {
+      if (storedSpecChars.length > 0) {
+        storedPassword.push(specialCharacters[Math.floor(Math.random() * specialCharacters.length)]);
+      }
+      else if (storedUpperCaseLetters.length > 0) {
+        storedPassword.push(capitalLetters[Math.floor(Math.random() * capitalLetters.length)]);
+      }
+      else if (storedLowerCaseLetters.length > 0) {
+        storedPassword.push(lowercaseLetters[Math.floor(Math.random() * lowercaseLetters.length)]);
+      }
+      else if (storedNumbers.length > 0) {
+        storedPassword.push(numbers[Math.floor(Math.random() * numbers.length)]);
+      }
+     } else {
+       storedPassword.join('');
+     }
+      alert("Congratulation! Your random Password is " + storedPassword);
+    } 
+   }
+  }
+ }    
+
+
+
+
+
+
+
             
 
-                function useLowerCase() {
-                  let storedLowerCaseLetters = prompt("Would you like to use Lower Case Letters? Please enter 'Yes' or 'No'");
-                    if (storedLowerCaseLetters == "Yes") {
-                      storedPassword.push(lowercaseLetters[Math.floor(Math.random() * lowercaseLetters.length)]);
-                  }
-                }
-
-              function useNumbers() {
-                  let storedNumbers = prompt("Would you like to use Numbers? Please enter 'Yes' or 'No'");
-                    if (storedNumbers == "Yes") {
-                      storedPassword.push(numbers[Math.floor(Math.random() * numbers.length)]);
-                  }
-                }  
-              if (storedPassword.length < passwordLength) {
-                storedPassword.push(specialCharacters[Math.floor(Math.random() * specialCharacters.length)]);
-                storedPassword.push(capitalLetters[Math.floor(Math.random() * capitalLetters.length)]);
-                storedPassword.push(lowercaseLetters[Math.floor(Math.random() * lowercaseLetters.length)]);
-                storedPassword.push(numbers[Math.floor(Math.random() * numbers.length)]);
-          } else {
-              storedPassword.join('');
-          }
-            alert("Congratulation! Your random Password is " + storedPassword.join('') + "!");
-        } 
-      }
 
 
 
